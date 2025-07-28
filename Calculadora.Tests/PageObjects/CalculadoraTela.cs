fbc1ff81a9bd09ff27fcb06762916b317f120176
Vitor Freitas <vitorfreitasjobs@gmail.com>
2025-07-28 10:35:30 -0300

    Initial commit of the project

---
+++ Calculadora.Tests/PageObjects/CalculadoraTela.cs
@@ -0,0 +1,29 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Appium;
+using OpenQA.Selenium.Appium.Windows;
+
+namespace Calculadora.Tests.PageObjects
+{
+    public class CalculadoraTela
+    {
+        private readonly WindowsDriver _driver;
+
+        public CalculadoraTela(WindowsDriver driver)
+        {
+            _driver = driver;
+        }
+
+        // Mapeamento dos elementos da tela usando a sintaxe completa
+        public AppiumElement BotaoSete => _driver.FindElement(By.XPath("//Button[@AutomationId='num7Button']"));
+        public AppiumElement BotaoOito => _driver.FindElement(By.XPath("//Button[@AutomationId='num8Button']"));
+        public AppiumElement BotaoSomar => _driver.FindElement(By.XPath("//Button[@AutomationId='plusButton']"));
+        public AppiumElement BotaoIgual => _driver.FindElement(By.XPath("//Button[@AutomationId='equalButton']"));
+        public AppiumElement VisorResultado => _driver.FindElement(By.XPath("//Text[@AutomationId='CalculatorResults']"));
+
+        public string ObterResultadoDoVisor()
+        {
+            return VisorResultado.Text.Replace("A exibição é", "").Trim();
+        }
+    }
+}
+
---
+++ Calculadora.Tests/Tests/CalculadoraSomaTests.cs
@@ -0,0 +1,66 @@
+﻿using Calculadora.Tests.PageObjects;
+using OpenQA.Selenium.Appium;
+using OpenQA.Selenium.Appium.Windows;
+
+namespace Calculadora.Tests.Tests
+{
+    [TestClass]
+    public sealed class CalculadoraSomaTests
+    {
+        private WindowsDriver _driver;
+        private const string WinAppDriverUrl = "http://127.0.0.1:4723";
+        // ID da calculadora do Windows 10/11
+        private const string CalculatorAppId = "Microsoft.WindowsCalculator_8wekyb3d8bbwe!App";
+
+        [TestInitialize]
+        public void Setup()
+        {
+            // Opções para iniciar a sessão com a Calculadora
+            var appiumOptions = new AppiumOptions();
+            appiumOptions.App = CalculatorAppId;
+            appiumOptions.DeviceName = "WindowsPC";
+
+            // Especifica que queremos usar o driver do Windows
+            appiumOptions.AutomationName = "Windows";
+
+            // Cria a sessão com a Calculadora
+            _driver = new WindowsDriver(new Uri(WinAppDriverUrl), appiumOptions);
+
+        }
+
+        [TestCleanup]
+        public void TearDown()
+        { 
+            // Fecha a sessão (e a Calculadora) após cada teste
+            if (_driver != null)
+            {
+                _driver.Quit();
+                _driver = null;
+            }
+               
+        }
+
+        [TestMethod]
+        public void DeveAbrirECarregarACalculadora()
+        {
+            // O teste mais simples: ele apenas verifica se o título da janela é "Calculadora"
+            Assert.AreEqual("Calculadora", _driver.Title);
+        }
+
+        [TestMethod]
+        public void DeveSomarSeteMaisOitoEResultarEmQuinze()
+        {
+            // 1. Arrange: Cria uma instância da nossa tela da Calculadora
+            var telaCalculadora = new CalculadoraTela(_driver);
+
+            // 2. Act: Executa as ações usando os elementos mapeados na tela
+            telaCalculadora.BotaoSete.Click();
+            telaCalculadora.BotaoSomar.Click();
+            telaCalculadora.BotaoOito.Click();
+            telaCalculadora.BotaoIgual.Click();
+
+            // 3. Assert: Pega o resultado do visor e verifica se está correto
+            Assert.AreEqual("15", telaCalculadora.ObterResultadoDoVisor());
+        }
+    }
+}
